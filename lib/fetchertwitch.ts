613199d186b222d7732cd24a3479ff5883de6486
Grant Robinson <Chisdeal2014@hotmail.com>
2022-10-12 01:05:36 +0100

    Update fetchertwitch.ts

--- lib/fetchertwitch.ts
+++ lib/fetchertwitch.ts
@@ -1,7 +1,7 @@
 const dev = process.env.NODE_ENV !== "production";
 export const host = dev ? "http://localhost:3000" : "https://clip.chisdealhd.co.uk";
 
-export const fetcher = async (query: string) =>
+export const fetchertwitch = async (query: string) =>
   await fetch(`${host}/api/gql`, {
     method: "POST",
     headers: {
