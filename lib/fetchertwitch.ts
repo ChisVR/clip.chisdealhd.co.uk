ccd26234c4c060f52d2bbc89da40653181004f8e
NekoSuneVR <Chisdeal2014@hotmail.com>
2023-04-24 15:41:12 +0100

    Update fetchertwitch.ts

--- lib/fetchertwitch.ts
+++ lib/fetchertwitch.ts
@@ -1,5 +1,5 @@
 const dev = process.env.NODE_ENV !== "production";
-export const host = dev ? "http://localhost:3000" : "https://clip.chisdealhd.co.uk";
+export const host = dev ? "http://localhost:3000" : "https://clip.nekosunevr.co.uk";
 
 export const fetcher = async (query: string) =>
   await fetch(`${host}/api/gql`, {
