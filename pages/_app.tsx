1c32d17f40d484188d055aeec8eede88f4535898
NekoSuneVR <chisdeal2014@hotmail.com>
2023-04-24 15:47:25 +0100

    Added Trackers

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import { AppProps } from "next/app";
 import React, { FunctionComponent } from "react";
+import { Analytics } from '@vercel/analytics/react';
 
 import { Layout } from "../components/layout";
 
@@ -22,6 +23,7 @@ const App: FunctionComponent<AppProps> = ({ Component, pageProps }) => {
     <ThemeProvider theme={theme}>
       <Layout>
         <Component {...pageProps} />
+        <Analytics />
       </Layout>
     </ThemeProvider>
   );
