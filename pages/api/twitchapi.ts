4590c82ae60a76ffffa407e40ac521b94d02e097
NekoSuneVR <Chisdeal2014@hotmail.com>
2023-12-31 02:08:43 +0000

    Update twitchapi.ts

--- pages/api/twitchapi.ts
+++ pages/api/twitchapi.ts
@@ -29,7 +29,7 @@ const updateClips = async (): Promise<void> => {
   )
     return;
   const data: Clips = await fetch(
-    `https://api.nekosunevr.co.uk/v4/social/api/twitch/clips/nekosunevr/100`,
+    `https://api.nekosunevr.co.uk/v4/social/api/twitch/ownerclips/list`,
     {
       headers: {
         'nekosunevr-api-key': process.env.NEKOSUNEAPIKEY,
